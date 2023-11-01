bb5fa1db4912ebd76130d57fa0b783e72ee3b33a
sebastian <sebastian.konkol2206@gmail.com>
2023-11-01 23:40:23 +0100

    table created successfully

--- plugins/mysql.ts
+++ plugins/mysql.ts
@@ -7,7 +7,8 @@ export default defineNuxtPlugin(() => {
         user: 'root',
         password: 'password',
         database: 'service',
-    })
+    });
+
     connection.connect();
 
     // Table users {
@@ -75,27 +76,98 @@ export default defineNuxtPlugin(() => {
 
 //Ref: users.id < follows.followed_user_id
 
+    // CLIENT
+    // Each location belongs to one client
+    connection.query(`
+        CREATE TABLE IF NOT EXISTS client (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL
+        );`
+    );
+
+    // LOCATION
+    // Many locations can belong to one client
+    connection.query(`
+        CREATE TABLE IF NOT EXISTS location (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            name VARCHAR(255) NOT NULL,
+            address VARCHAR(255) NOT NULL,
+            client INT NOT NULL,
+            FOREIGN KEY (client) REFERENCES client(id)
+        );`
+    );
+
+    // USER
+    // Many users can belong to one client as their employee
     connection.query(`
         CREATE TABLE IF NOT EXISTS user (
-            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            id INT PRIMARY KEY AUTO_INCREMENT,
             email VARCHAR(254) NOT NULL,
             password CHAR(64) NOT NULL,
             name VARCHAR(50) NOT NULL,
             surname VARCHAR(50) NOT NULL,
             role ENUM('ROLE_ADMIN', 'ROLE_OFFICE', 'ROLE_USER') NOT NULL,
-            employer INT NOT NULL
+            employer INT NOT NULL,
+            FOREIGN KEY (employer) REFERENCES client(id)
         );`
     );
 
+    // REPORT
+    // Each report is associated with one location and one creator (user)
+    connection.query(`
+        CREATE TABLE IF NOT EXISTS report (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            title VARCHAR(255) NOT NULL,
+            content TEXT NOT NULL,
+            status ENUM('OPEN', 'IN_PROGRESS', 'RESOLVED', 'DUPLICATE') NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            location_id INT NOT NULL,
+            created_by INT NOT NULL,
+            FOREIGN KEY (location_id) REFERENCES location(id),
+            FOREIGN KEY (created_by) REFERENCES user(id)
+        );`
+    );
+
+    // NOTIFICATION
+    // Each notification is linked to one report
     connection.query(`
         CREATE TABLE IF NOT EXISTS notification (
-            id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            id INT PRIMARY KEY AUTO_INCREMENT,
             content VARCHAR(255),
             report_id INT NOT NULL,
             FOREIGN KEY (report_id) REFERENCES report(id)
         );`
     );
 
+    // COMMENT
+    // Each comment is associated with one report and one creator (user)
+    connection.query(`
+        CREATE TABLE IF NOT EXISTS comment (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            content TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            report_id INT NOT NULL,
+            created_by INT NOT NULL,
+            FOREIGN KEY (report_id) REFERENCES report(id),
+            FOREIGN KEY (created_by) REFERENCES user(id)
+        );`
+    );
+
+    // FILE
+    // Each file is linked optionally to one report or comment
+    connection.query(`
+        CREATE TABLE IF NOT EXISTS file (
+            id INT PRIMARY KEY AUTO_INCREMENT,
+            filename VARCHAR(255) NOT NULL,
+            report_id INT,
+            comment_id INT,
+            FOREIGN KEY (report_id) REFERENCES report(id),
+            FOREIGN KEY (comment_id) REFERENCES comment(id)
+        );`
+    );
+
+    // USER_NOTIFICATION
+    // A junction table connecting users and notifications to track who receives notifications
     connection.query(`
         CREATE TABLE IF NOT EXISTS user_notification (
             user_id INT,
@@ -106,15 +178,8 @@ export default defineNuxtPlugin(() => {
         );`
     );
 
-    connection.query(`
-        CREATE TABLE IF NOT EXISTS report (
-            id INTEGER PRIMARY KEY AUTO_INCREMENT,
-            title VARCHAR(255) NOT NULL,
-            content TEXT NOT NULL,
-            status ENUM('OPEN', 'IN_PROGRESS', 'RESOLVED', 'DUPLICATE') NOT NULL
-        );`
-    );
-
+    //REPORT_HANDLED_BY
+    // A junction table linking reports and users to track who handled a specific report
     connection.query(`
         CREATE TABLE report_handled_by (
             report_id INT,
@@ -122,15 +187,6 @@ export default defineNuxtPlugin(() => {
             PRIMARY KEY (report_id, user_id),
             FOREIGN KEY (report_id) REFERENCES report(id),
             FOREIGN KEY (user_id) REFERENCES user(id)
-        );`
-    );
-
-    connection.query(`
-        CREATE TABLE IF NOT EXISTS file (
-            id INT PRIMARY KEY,
-            filename VARCHAR(255),
-            report_id INT,
-            FOREIGN KEY (report_id) REFERENCES reports(report_id)
         );`
     );
 
