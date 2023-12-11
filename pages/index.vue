bfb5cddc93026835a68c2cda1538e7bb5759befa
MaciejW <macwal1234@gmail.com>
2023-12-11 14:40:04 +0100

    merge with dev

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 
 import ReportList from "~/components/dashboard/report/ReportList.vue";
-import useAuthStore from "~/stores/auth";
-const store = useAuthStore()
-if (!store.isLoggedIn()){
-  navigateTo('/')
-}
+
 </script>
 <template>
       <report-list></report-list>
