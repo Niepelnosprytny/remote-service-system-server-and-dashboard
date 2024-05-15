183495a7fcbf087e44f0b34d3e8ae0e5ec326c6a
Niepelnosprytny <sebastian.konkol2206@gmail.com>
2024-05-15 17:11:13 +0200

    websocket fix

--- plugins/weboscket.ts
+++ plugins/weboscket.ts
@@ -2,10 +2,14 @@ import {useWebSocket} from "@vueuse/core";
 export default defineNuxtPlugin({
     name: 'WS',
     setup () {
-        const notificationWS = useWebSocket('/api/websockets/notification')
-        const chatWS = useWebSocket(`/api/websockets/chatroom`)
-        const adminPanelWS = useWebSocket(`/api/websockets/adminPanel`)
-        const reportListWS = useWebSocket(`/api/websockets/reportList`)
+        const notificationWS = useWebSocket('ws://0.0.0.0:3000/api/websockets/notification')
+        //const notificationWS = useWebSocket('wss://sebastianinc.toadres.pl/api/websockets/notification')
+        const chatWS = useWebSocket(`ws://0.0.0.0:3000/api/websockets/chatroom`)
+        //const chatWS = useWebSocket(`wss://sebastianinc.toadres.pl/api/websockets/chatroom`)
+        const adminPanelWS = useWebSocket(`ws://0.0.0.0:3000/api/websockets/adminPanel`)
+        //const adminPanelWS = useWebSocket(`wss://sebastianinc.toadres.pl/api/websockets/adminPanel`)
+        const reportListWS = useWebSocket(`ws://0.0.0.0:3000/api/websockets/reportList`)
+        //const reportListWS = useWebSocket(`wss://sebastianinc.toadres.pl/api/websockets/reportList`)
 
 
         return {
