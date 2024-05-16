382e931f63472b5a95b726ba6c826d6b4bfbfaed
Niepelnosprytny <sebastian.konkol2206@gmail.com>
2024-05-16 20:47:37 +0200

    actually fixed websocket

--- plugins/weboscket.ts
+++ plugins/weboscket.ts
@@ -2,15 +2,12 @@ import {useWebSocket} from "@vueuse/core";
 export default defineNuxtPlugin({
     name: 'WS',
     setup () {
-        const notificationWS = useWebSocket('ws://0.0.0.0:3000/api/websockets/notification')
-        //const notificationWS = useWebSocket('wss://sebastianinc.toadres.pl/api/websockets/notification')
-        const chatWS = useWebSocket(`ws://0.0.0.0:3000/api/websockets/chatroom`)
-        //const chatWS = useWebSocket(`wss://sebastianinc.toadres.pl/api/websockets/chatroom`)
-        const adminPanelWS = useWebSocket(`ws://0.0.0.0:3000/api/websockets/adminPanel`)
-        //const adminPanelWS = useWebSocket(`wss://sebastianinc.toadres.pl/api/websockets/adminPanel`)
-        const reportListWS = useWebSocket(`ws://0.0.0.0:3000/api/websockets/reportList`)
-        //const reportListWS = useWebSocket(`wss://sebastianinc.toadres.pl/api/websockets/reportList`)
+        const host = "wss://sebastianinc.toadres.pl";
 
+        const notificationWS = useWebSocket(`${host}/api/websockets/notification`)
+        const chatWS = useWebSocket(`${host}/api/websockets/chatroom`)
+        const adminPanelWS = useWebSocket(`${host}/api/websockets/adminPanel`)
+        const reportListWS = useWebSocket(`${host}/api/websockets/reportList`)
 
         return {
             provide: {
